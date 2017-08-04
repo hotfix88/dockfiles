18b762beb726452b28ae6e8f3145ff1e50dd04c4
hotfix88 <hotfix88@sina.com>
2017-08-04 16:18:44 +0800

    1111

--- helloworld.c
+++ helloworld.c
@@ -3,18 +3,9 @@
 //fengyang test 
 int main()
 {
-    int a,b;
+
     printf("hello world!\n");
-    
-    //Öµ´«µÝ 
-    a = 100;
-    b = a;
-    a = 200;
-    printf("%d,%d\n",a,b);
-    
-    
-    
-    getchar();
-    return 1;
+    return 0;
+   
 }
     
