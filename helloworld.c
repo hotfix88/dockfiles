e975353087629ebc78131cbde60019b018fa157e
hotfix88 <hotfix88@sina.com>
2017-08-04 12:57:57 +0800

    hello

---
+++ helloworld.c
@@ -0,0 +1,20 @@
+ 
+#include <stdio.h>
+//fengyang test 
+int main()
+{
+    int a,b;
+    printf("hello world!\n");
+    
+    //Öµ´«µÝ 
+    a = 100;
+    b = a;
+    a = 200;
+    printf("%d,%d\n",a,b);
+    
+    
+    
+    getchar();
+    return 1;
+}
+    
